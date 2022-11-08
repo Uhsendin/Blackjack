51edbfa9f825b45497c1c2c5f347839d8d92bdc0
Uhsendin <chilebr99@gmail.com>
2022-11-08 11:45:23 -0500

    Added start game & new card buttons with CSS

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Whoo! You've got Blackjack"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+
+function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + "," + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "Whoo! You've got Blackjack";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message
 }
 
-console.log(message)
 
-
+function newCard() {
+    console.log("Drawing a new card form the deck!")
+}
